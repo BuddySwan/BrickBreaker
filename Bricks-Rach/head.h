ea35d8039f0f0e11e094d66106885096e7b69b16
BuddySwan <46689781+BuddySwan@users.noreply.github.com>
2019-04-16 17:40:54 -0400

    Update head.h

--- Bricks-Rach/head.h
+++ Bricks-Rach/head.h
@@ -1,7 +1,7 @@
 #include<SDL2/SDL.h>
 
-//#include<SDL2/SDL_image.h>
-#include<SDL2_image/SDL_image.h>
+#include<SDL2/SDL_image.h>
+//#include<SDL2_image/SDL_image.h>
 #include<cstdio>
 #include<string>
 #include<vector>
